99d62b976db862bac05f902f4a741133eef24bb4
kothari-vanshika <vanshikakothari524@gmail.com>
2025-04-01 22:25:51 +0530

    updated debug value

--- flask-plant-disease/main.py
+++ flask-plant-disease/main.py
@@ -111,8 +111,6 @@ def get_disease_info():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))  # Use Render's default port (10000)
-    app.run(host='0.0.0.0', port=port, debug=True)
-# if __name__ == '__main__':
-# #     port = int(os.environ.get('PORT', 10000))  # Use Render's default port (10000)
-#      app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
+
 
